c9bc22b1d4929522808f5698374e37391515db04
Keith C. Ivey <keith@iveys.org>
2018-01-31 18:05:17 -0500

    use sqlite database and CSV of current committees

--- load-data.js
+++ load-data.js
@@ -1,63 +1,141 @@
-var csv = require('csv'),
+#!/usr/bin/env node
+
+var fs = require('fs'),
+    parse = require('csv-parse'),
     _ = require('underscore'),
-    async = require('async'),
-    mysql = require('mysql'),
     moment = require('moment'),
+    knex = require('knex')({
+        client: 'sqlite3',
+        connection: {
+            filename: __dirname +  '/dc-ocf.sqlite'
+        }
+    }),
     csvOptions = {columns: true},
     config = require('./config'),
-    db = mysql.createConnection(config.database),
-    queue = async.queue(insertRecord);
+    currentCommittees = {},
+    batchSize = 10;
 
-db.connect(function (err) {
-    if (err) {
-        throw err;
-    }
-});
-
-queue.drain = function () {
-    console.log('all records inserted');
-    db.end(function (err) {
-        if (err) {
-            throw err;
-        }
-        console.log('db connection closed');
-    });
-};
-
-csv()
-    .from.path(__dirname + '/contributions.csv', csvOptions)
-    .transform(function (row) {
-        var newRow = {};
-        _.each(row, function (value, key) {
-            newRow[nameToCode(key)] = trim(value);
-        });
-        newRow.date_of_receipt =
-            moment(newRow.date_of_receipt).format('YYYY-MM-DD');
-        newRow.amount = newRow.amount.replace(/[$,]/g, '');
-        return newRow;
-    })
-    .on('record', function (row, index) {
-        queue.push(row, function (err) {
-            if (err) {
-                throw err;
+knex.schema.createTable(
+    'contributions',
+    function (table) {
+        var columnNames = [
+                'committee_name',
+                'contributor_name',
+                'contributor_address',
+                'contributor_type',
+                'contribution_type',
+                'employer_name',
+                'employer_address',
+                'occupation',
+                'receipt_date',
+                'amount'
+            ];
+        table.increments();
+        columnNames.forEach(function (columnName) {
+            if (/date/.test(columnName)) {
+                table.date(columnName);
+            }
+            else if (/amount|total/.test(columnName)) {
+                table.float(columnName);
+            }
+            else {
+                table.string(columnName);
             }
         });
-    })
-    .on('close', function (count) {
-        console.log('Total: ', count);
-    })
-    .on('error', function (err) {
+    }
+)
+.then(function () {
+    readCommittees();
+});
+
+function readCommittees() {
+    var parser = parse(csvOptions),
+        input = fs.createReadStream(__dirname + '/committees.csv');
+    parser.on('readable', function () {
+        var record;
+        while(record = parser.read()) {
+            record = transformRecord(record);
+            currentCommittees[record.committee_name] = true;
+        }
+    });
+    parser.on('error', function (err) {
+        console.error(err.message);
         throw err;
     });
+    parser.on('finish', function () {
+        console.log('Finished reading committees');
+        console.log(Object.keys(currentCommittees).sort());
+        readContributions();
+    });
+    input.pipe(parser);
+}
 
-function insertRecord(row, callback) {
-    db.query('INSERT INTO contributions SET ?', [row], callback);
+function readContributions() {
+    var tableName = 'contributions',
+        parser = parse(csvOptions),
+        input = fs.createReadStream(__dirname + '/' + tableName + '.csv'),
+        seen = {},
+        unrecognized = [],
+        totalCount = 0,
+        currentCount = 0,
+        batch = [];
+    parser.on('readable', function () {
+        var record;
+        while (record = parser.read()) {
+            var name;
+            totalCount++;
+            record = transformRecord(record);
+            name = record.committee_name;
+            if (!seen[name]) {
+                seen[name] = true;
+                console.log(name);
+                if (!currentCommittees[name]) {
+                    unrecognized.push(name);
+                }
+            }
+            if (!currentCommittees[name]) {
+                continue;
+            }
+            batch.push(record);
+            if (batch.length >= batchSize) {
+                batchInsert(batch);
+                batch = [];
+            }
+            currentCount++;
+        }
+    });
+    parser.on('error', function (err) {
+        console.error(err.message);
+        throw err;
+    });
+    parser.on('finish', function () {
+        if (batch.length) {
+            batchInsert(batch);
+        }
+        console.log('Finished reading %d contributions', totalCount);
+        console.log('Inserted %d contributions', currentCount);
+        console.log('Unrecognized committees:\n', unrecognized.sort());
+    });
+    input.pipe(parser);
+}
+
+function transformRecord(record) {
+    var newRecord = {};
+    _.each(record, function (value, key) {
+        value = trim(value);
+        key = nameToCode(key);
+        if (/date/.test(key) && /^\d\d?\/\d\d?\/\d{4}$/.test(value)) {
+            value = moment(value, 'MM/DD/YYYY').format('YYYY-MM-DD');
+        }
+        else if (/amount|total/.test(key) && /^[\$\.,\d]+$/.test(value)) {
+            value = value.replace(/[\$,]/g, '');
+        }
+        newRecord[key] = value;
+    });
+    return newRecord;
 }
 
 function nameToCode(s) {
-    if (s == null) {
-        return null;
-    }
     return s.toLowerCase()
         .replace(/\W+/g, '_')
         .replace(/^_|_$/g, '');
@@ -69,4 +147,10 @@ function trim(s) {
     }
     return s.replace(/\s+/g, ' ')
         .replace(/^ | $/g, '');
+}
+
+function batchInsert(batch) {
+    knex.batchInsert('contributions', batch, batchSize)
+        .returning('id')
+        .then(function () {});
 }

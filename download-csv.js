542acff59e6a76b4ee99ba38a7b0b0ddb4fc7c8f
Keith C. Ivey <keith@iveys.org>
2019-03-10 11:08:12 -0400

    more work on download-csv.js (I think a module bug is preventing downloading the contributions CSV)

--- download-csv.js
+++ download-csv.js
@@ -6,30 +6,63 @@ const Browser = require('zombie');
 const browser = new Browser({waitDuration: '30s'});
 const electionYear = '2018';
 
-browser.visit('https://efiling.ocf.dc.gov/Disclosure')
-    .then(() => browser.select('#FilerTypeId', 'Principal Campaign Committee'))
-    .then(() => browser.select('#ElectionYear', electionYear))
-    .then(pause)
-    .then(() => browser.click('#btnSubmitSearch'))
-    .then(getCsv)
-    .then(csv => fs.writeFileSync(__dirname + '/committees.csv', csv, {encoding: 'utf-8'}));
+// Force 5s pause between requests
+browser.pipeline.addHandler(function(browser, request) {
+    // Log the response body
+    return new Promise(resolve => setTimeout(resolve, 5000));
+});
+
+writeCommitteeCsv();
+
+function writeCommitteeCsv() {
+    const file =__dirname + '/committees.csv';
+    return browser.visit('https://efiling.ocf.dc.gov/Disclosure')
+        .then(() => browser.select('#FilerTypeId', 'Principal Campaign Committee'))
+        .then(() => browser.select('#ElectionYear', electionYear))
+        .then(() => browser.click('#btnSubmitSearch'))
+        .then(getCsv)
+        .then(csv => fs.writeFileSync(file, csv, {encoding: 'utf-8'}));
+}
+
+function writeContributionCsv() {
+    const file =__dirname + '/contributions.csv';
+    return browser.visit('https://efiling.ocf.dc.gov/ContributionExpenditure')
+        .then(() => browser.select('#FilerTypeId', 'Principal Campaign Committee'))
+        .then(() => browser.click('#contributions'))
+        .then(() => browser.click('#accordionpanel4 a'))
+        .then(() => browser.fill('#FromDate', '01/01/2016'))
+        .catch(function(err) {
+            if (err.message === "Cannot read property 'settings' of undefined") {
+                return null;
+            }
+            throw err;
+        })
+        .then(() => browser.click('#btnSubmitSearch'))
+        .then(getCsv)
+        .then(csv => fs.writeFileSync(file, csv, {encoding: 'utf-8'}));
+}
 
 function pause() {
     return new Promise(resolve => setTimeout(resolve, 5000));
 }
 
 function getCsv() {
-    return browser
-        .click('#divExportDropdown')
-        .then(pause)
+    return browser.click('#divExportDropdown')
         .then(() => browser.click('#exportDropDown > li:last-child > a'))
-        .then(() => Buffer.from(browser.tabs[1].resources[0].response.body))
-        .then(function (buf) {
-            browser.tabs[1].close();
+        .then(function () {
+            console.log(browser.tabs.length + ' tabs');
+            const lastTab = browser.tabs[browser.tabs.length - 1];
+            const buf = Buffer.from(lastTab.resources[0].response.body);
+            console.log('Got buffer', buf.length);
+            lastTab.close();
             return buf;
         })
-        // Convert UTF-16 to UTF-8
-        .then(buf => new util.TextDecoder('utf-16').decode(buf))
-        // Fix line endings and delete first line (which is not CSV)
-        .then(csv => csv.replace(/\r\n/, '\n').replace(/^.+\n/, ''));
+        .then(function (buf) {
+            console.log('processing buffer');
+            // Convert UTF-16 to UTF-8
+            let csv = new util.TextDecoder('utf-16').decode(buf);
+            // Fix line endings and delete first line (which is not CSV)
+            return csv.replace(/\r\n/, '\n')
+                .replace(/^.+\n/, '');
+        });
 }

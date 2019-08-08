49a32656fce7a8871c9aff3fbb76dd4b0d6dc8f8
Keith C. Ivey <keith@iveys.org>
2019-08-08 10:38:41 -0400

    move all database calls from load-data.js into db.js

--- load-data.js
+++ load-data.js
@@ -8,49 +8,6 @@ const db = require('./lib/db');
 const csvOptions = {columns: true};
 const currentCommittees = {};
 const batchSize = 10;
-const committeeTableName = 'committees';
-const committeeColumns = [
-    'committee_name',
-    'candidate_name',
-    'election_year',
-    'status',
-    'office',
-];
-const contributionTableName = 'contributions';
-const contributionColumns = [
-    'committee_name',
-    'contributor_first_name',
-    'contributor_middle_name',
-    'contributor_last_name',
-    'number_and_street',
-    'contributor_organization_name',
-    'city',
-    'state',
-    'zip',
-    'normalized',
-    'contributor_type',
-    'contribution_type',
-    'employer_name',
-    'employer_address',
-    'occupation',
-    'receipt_date',
-    'amount',
-];
-const expenditureTableName = 'expenditures';
-const expenditureColumns = [
-    'committee_name',
-    'payee_first_name',
-    'payee_middle_name',
-    'payee_last_name',
-    'number_and_street',
-    'city',
-    'state',
-    'zip',
-    'normalized',
-    'purpose_of_expenditure',
-    'payment_date',
-    'amount',
-];
 const abbrev = {
     STREET: 'ST',
     ROAD: 'RD',
@@ -66,62 +23,11 @@ const abbrev = {
 };
 const abbrevRegexp = new RegExp('\\b(' + Object.keys(abbrev).join('|') + ')\\b');
 
-db.schema.dropTableIfExists(contributionTableName)
-    .dropTableIfExists(expenditureTableName)
-    .dropTableIfExists(committeeTableName)
-    .createTable(
-        committeeTableName,
-        function (table) {
-            table.increments();
-            committeeColumns.forEach(function (columnName) {
-                if (/year/.test(columnName)) {
-                    table.integer(columnName);
-                }
-                else {
-                    table.string(columnName);
-                }
-            });
-        }
-    )
-    .createTable(
-        contributionTableName,
-        function (table) {
-            table.increments();
-            contributionColumns.forEach(function (columnName) {
-                if (/date/.test(columnName)) {
-                    table.date(columnName);
-                }
-                else if (/amount|total/.test(columnName)) {
-                    table.float(columnName);
-                }
-                else {
-                    table.string(columnName);
-                }
-            });
-        }
-    )
-    .createTable(
-        expenditureTableName,
-        function (table) {
-            table.increments();
-            expenditureColumns.forEach(function (columnName) {
-                if (/date/.test(columnName)) {
-                    table.date(columnName);
-                }
-                else if (/amount|total/.test(columnName)) {
-                    table.float(columnName);
-                }
-                else {
-                    table.string(columnName);
-                }
-            });
-        }
-    )
-    .then(readCommittees);
+db.createTables().then(readCommittees);
 
 function readCommittees() {
     const parser = parse(csvOptions);
-    const input = fs.createReadStream(__dirname + '/' + committeeTableName + '.csv');
+    const input = fs.createReadStream(__dirname + '/' + db.committeeTableName + '.csv');
     const records = [];
     parser.on('readable', function () {
         let checked = false;
@@ -129,8 +35,8 @@ function readCommittees() {
         while ((record = parser.read())) {
             record = transformRecord(record);
             if (!checked) {
-                if (!arraysAreEqual(committeeColumns, Object.keys(record))) {
-                    console.warn(committeeColumns, Object.keys(record));
+                if (!arraysAreEqual(db.committeeColumns, Object.keys(record))) {
+                    console.warn(db.committeeColumns, Object.keys(record));
                     throw new Error('Committee columns have changed');
                 }
                 checked = true;
@@ -146,8 +52,7 @@ function readCommittees() {
     parser.on('finish', function () {
         console.log('Finished reading committees');
         console.log(Object.keys(currentCommittees).sort());
-        db.batchInsert(committeeTableName, records, batchSize)
-            .returning('id')
+        db.batchInsertCommittees(records, batchSize)
             .then(readContributions);
     });
     input.pipe(parser);
@@ -155,7 +60,7 @@ function readCommittees() {
 
 function readContributions() {
     const parser = parse(csvOptions);
-    const input = fs.createReadStream(__dirname + '/' + contributionTableName + '.csv');
+    const input = fs.createReadStream(__dirname + '/' + db.contributionTableName + '.csv');
     const seen = {};
     const unrecognized = [];
     let totalCount = 0;
@@ -167,7 +72,7 @@ function readContributions() {
             totalCount++;
             record = transformRecord(record);
             if (totalCount === 1) {
-                const expected = contributionColumns.filter(v => v !== 'normalized');
+                const expected = db.contributionColumns.filter(v => v !== 'normalized');
                 if (!arraysAreEqual(expected, Object.keys(record))) {
                     console.warn(expected, Object.keys(record));
                     throw new Error('Contribution columns have changed');
@@ -187,7 +92,7 @@ function readContributions() {
             record.normalized = normalizeNameAndAddress(makeName(record), makeAddress(record));
             batch.push(record);
             if (batch.length >= 10000) {
-                db.batchInsert(contributionTableName, batch, batchSize)
+                db.batchInsertContributions(batch, batchSize)
                     .then(function () {});
                 batch = [];
             }
@@ -200,7 +105,7 @@ function readContributions() {
     });
     parser.on('finish', function () {
         if (batch.length) {
-            db.batchInsert(contributionTableName, batch, batchSize)
+            db.batchInsertContributions(batch, batchSize)
                 .then(function () {});
         }
         console.log('Finished reading %d contributions', totalCount);
@@ -213,7 +118,7 @@ function readContributions() {
 
 function readExpenditures() {
     const parser = parse(csvOptions);
-    const input = fs.createReadStream(__dirname + '/' + expenditureTableName + '.csv');
+    const input = fs.createReadStream(__dirname + '/' + db.expenditureTableName + '.csv');
     const seen = {};
     const unrecognized = [];
     let totalCount = 0;
@@ -225,7 +130,7 @@ function readExpenditures() {
             totalCount++;
             record = transformRecord(record);
             if (totalCount === 1) {
-                const expected = expenditureColumns.filter(v => v !== 'normalized');
+                const expected = db.expenditureColumns.filter(v => v !== 'normalized');
                 if (!arraysAreEqual(expected, Object.keys(record))) {
                     console.warn(expected, Object.keys(record));
                     throw new Error('Expenditure columns have changed');
@@ -245,7 +150,7 @@ function readExpenditures() {
             record.normalized = normalizeNameAndAddress(makeName(record), makeAddress(record));
             batch.push(record);
             if (batch.length >= 10000) {
-                db.batchInsert(expenditureTableName, batch, batchSize)
+                db.batchInsertExpenditures(batch, batchSize)
                     .then(function () {});
                 batch = [];
             }
@@ -258,7 +163,7 @@ function readExpenditures() {
     });
     parser.on('finish', function () {
         if (batch.length) {
-            db.batchInsert(expenditureTableName, batch, batchSize)
+            db.batchInsertExpenditures(batch, batchSize)
                 .then(finish);
         }
         else {
@@ -271,51 +176,10 @@ function readExpenditures() {
             if (unrecognized.length) {
                 console.log('Unrecognized committees:\n', unrecognized.sort());
             }
-            addDummyContributions();
+            db.addDummyContributions();
         }
     });
     input.pipe(parser);
-}
-
-// Add negative contributions corresponding to refunds and bounced checks
-function addDummyContributions() {
-    db
-        .select(
-            'e.committee_name',
-            'e.payee_first_name as contributor_first_name',
-            'e.payee_middle_name as contributor_middle_name',
-            'e.payee_last_name as contributor_last_name',
-            'e.number_and_street',
-            'e.city',
-            'e.state',
-            'e.zip',
-            'e.normalized',
-            'c.contributor_type',
-            'e.purpose_of_expenditure as contribution_type',
-            'c.employer_name',
-            'c.employer_address',
-            'c.occupation',
-            'e.payment_date as receipt_date',
-            db.raw('-e.amount as amount')
-        )
-        .from('expenditures as e')
-        .innerJoin('contributions as c', function () {
-            this.on('e.committee_name', 'c.committee_name')
-                .andOn('e.normalized', 'c.normalized');
-        })
-        .whereIn('purpose_of_expenditure', ['Refund', 'Return Check and Fees'])
-        .groupBy('e.id')
-        .orderBy('e.committee_name')
-        .orderBy('e.payee_last_name')
-        .orderBy('e.payee_first_name')
-        .orderBy('e.payee_middle_name')
-        .then(function (rows) {
-            db.batchInsert(contributionTableName, rows, batchSize)
-                .then(function () {
-                    console.log('Dummy contributions inserted');
-                    process.exit();
-                });
-        });
 }
 
 function transformRecord(record) {

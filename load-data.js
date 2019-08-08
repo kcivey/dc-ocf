cf3f24fb327b589c3f8425c44b7e0e979e402064
Keith C. Ivey <keith@iveys.org>
2019-08-08 16:44:47 -0400

    move some sharable functions out of load-data.js to util.js

--- load-data.js
+++ load-data.js
@@ -3,25 +3,13 @@
 const fs = require('fs');
 const parse = require('csv-parse');
 const _ = require('underscore');
+const underscored = require('underscore.string/underscored');
 const moment = require('moment');
 const db = require('./lib/db');
+const {fixAmount, fixDate, normalizeNameAndAddress} = require('./lib/util');
 const csvOptions = {columns: true};
 const currentCommittees = {};
 const batchSize = 10;
-const abbrev = {
-    STREET: 'ST',
-    ROAD: 'RD',
-    DRIVE: 'DR',
-    AVENUE: 'AVE',
-    COURT: 'CT',
-    LANE: 'LN',
-    TERRACE: 'TER',
-    CIRCLE: 'CIR',
-    BOULEVARD: 'BLVD',
-    HIGHWAY: 'HWY',
-    PLACE: 'PL',
-};
-const abbrevRegexp = new RegExp('\\b(' + Object.keys(abbrev).join('|') + ')\\b');
 
 db.createTables().then(readCommittees);
 
@@ -89,7 +77,7 @@ function readContributions() {
             if (!currentCommittees[name]) {
                 continue;
             }
-            record.normalized = normalizeNameAndAddress(makeName(record), makeAddress(record));
+            record.normalized = normalizeNameAndAddress(record);
             batch.push(record);
             if (batch.length >= 10000) {
                 db.batchInsertContributions(batch, batchSize)
@@ -147,7 +135,7 @@ function readExpenditures() {
             if (!currentCommittees[name]) {
                 continue;
             }
-            record.normalized = normalizeNameAndAddress(makeName(record), makeAddress(record));
+            record.normalized = normalizeNameAndAddress(record);
             batch.push(record);
             if (batch.length >= 10000) {
                 db.batchInsertExpenditures(batch, batchSize)
@@ -186,91 +174,20 @@ function transformRecord(record) {
     const newRecord = {};
     _.each(record, function (value, key) {
         value = trim(value);
-        key = nameToCode(key);
+        key = underscored(key);
         if (/date/.test(key) && /^\d\d?\/\d\d?\/\d{4}$/.test(value)) {
-            value = moment(value, 'MM/DD/YYYY').format('YYYY-MM-DD');
+            value = fixDate(value);
         }
         else if (/amount|total/.test(key) && /^[$.,\d]+$/.test(value)) {
-            value = value.replace(/[$,]/g, '');
+            value = fixAmount(value);
         }
         newRecord[key] = value;
     });
     return newRecord;
 }
 
-function nameToCode(s) {
-    return s.toLowerCase()
-        .replace(/\W+/g, '_')
-        .replace(/^_|_$/g, '');
-}
-
 function trim(s) {
-    return s ? s.replace(/\s+/g, ' ').replace(/^ | $/g, '') : '';
-}
-
-function makeName(r) {
-    const prefix = r.hasOwnProperty('contributor_name') ? 'contributor_' : 'payee_';
-    return ['first_name', 'middle_name', 'last_name', 'organization_name']
-        .map(c => r[prefix + c])
-        .filter(v => !!v)
-        .join(' ');
-}
-
-function makeAddress(r) {
-    let address = r.number_and_street || '';
-    if (r.city) {
-        if (address) {
-            address += ', ';
-        }
-        address += r.city;
-    }
-    if (r.state) {
-        if (address) {
-            address += ', ';
-        }
-        address += r.state;
-    }
-    if (r.zip) {
-        if (address) {
-            address += ' ';
-        }
-        address += r.zip;
-    }
-    return address;
-}
-
-function normalizeNameAndAddress(name, address) {
-    let normalized = name.toUpperCase()
-        .replace(/[ ,]*,[ ,]*/g, ' ')
-        .replace(/\./g, '')
-        .replace(/^(MR|MS|MRS|DR) /, '')
-        .replace(/ AND /g, ' & ')
-        .replace(/^THE /, '')
-        // .replace(/ [A-Z] /g, ' ') // remove middle initials
-        // .replace(/ (JR|SR|I{1,3})$/, '')
-        .replace(/[\- ]*-[\- ]*/g, ' ');
-    if (address) {
-        normalized += ', ' + address.toUpperCase()
-            .replace(/[ ,]*,[ ,]*/g, ' ')
-            .replace(/\./g, '')
-            .replace(/'/g, '')
-            .replace(/,?\s*([NS][EW],)/, ' $1')
-            .replace(/ [\d \-]+$/, '') // remove zip
-            .replace(/[\- ]*-[\- ]*/g, ' ')
-            .replace(/\b(SUITE|STE|APT|UNIT)[ #]+/, '#')
-            .replace(/# /, '#')
-            .replace(/ FL(?:OOR)? \d\d?(?:[NR]?D|ST|TH)? /, ' ')
-            .replace(/ \d\d?(?:[NR]?D|ST|TH)? FL(?:OOR)? /, ' ')
-            .replace(/ VIRGINIA$/, ' VA')
-            .replace(/ MARYLAND$/, ' MD')
-            .replace(/ DISTRICT OF COLUMBIA$/, ' DC')
-            .replace(/ MC LEAN /g, ' MCLEAN ')
-            .replace(/( \w+)(\1 [A-Z]{2})$/, '$2')
-            .replace(/( \w+ [A-Z]{2})\1$/, '$1')
-            .replace(/ #\S+/, '') // remove apartment number
-            .replace(abbrevRegexp, (m, p1) => abbrev[p1]);
-    }
-    return normalized;
+    return s ? s.replace(/\s+/g, ' ').trim() : '';
 }
 
 function arraysAreEqual(array1, array2) {

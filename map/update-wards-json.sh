d43d5eb78f23fcaa914035636b4cbb39689d3619
Keith C. Ivey <keith@iveys.org>
2019-08-13 22:08:41 -0400

    use better ward geojson

---
+++ map/update-wards-json.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+ORIGINAL_FILE=original.json
+TARGET_FILE=dc-wards.json
+if [ ! -e $ORIGINAL_FILE ]
+    then curl https://opendata.arcgis.com/datasets/0ef47379cbae44e88267c01eaec2ff6e_31.geojson -o $ORIGINAL_FILE
+fi
+npx geo2topo wards=$ORIGINAL_FILE \
+    | npx toposimplify -P 0.02 \
+    | npx topo2geo wards=- \
+    | json -o json-0 -e 'this.features.forEach(f=>f.properties={name:f.properties.NAME})' \
+    > $TARGET_FILE
+echo "$TARGET_FILE written"

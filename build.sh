cfab97fc523a51129fce5ab8ac91a877a268a332
Keith C. Ivey <keith@iveys.org>
2019-09-04 21:59:41 -0400

    add to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+./make-json.js
+echo 'Copying files from src to dist'
 mkdir -p dist
-cp -u src/* dist/
+rm dist/*
+cp -u src/*.{js,json,html} dist/
+echo 'Compiling and minifying JS'
 npx babel src/dc-campaign-finance.js | \
     npx browserify - | \
     npx terser -c -m --toplevel --comments /Copyright/ \

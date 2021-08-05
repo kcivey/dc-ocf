befa3c90743b41ecdf49e4fd5234c9aaf9907700
Keith C. Ivey <keith@iveys.org>
2021-08-05 17:31:34 -0400

    update download-fair-elections.js for site changes

--- download-fair-elections.js
+++ download-fair-elections.js
@@ -31,12 +31,15 @@ async function main() {
     if (argv.committee) {
         await browser.fill('input[placeholder="Search by Committee Name"]', argv.committee);
     }
-    let page = 1;
-    doTable: while (true) {
+    const buttons = await browser.querySelectorAll('ul.pagination > li.page-item > button');
+    const lastPage = +buttons.item(buttons.length - 2).textContent; // next-to-last button (before "next")
+    console.warn(`${lastPage} pages of results`);
+    for (let page = 1; page <= lastPage; page++) {
+        console.log(`Page ${page}`);
         const rows = await browser.querySelectorAll('table > tbody > tr');
         for (const row of rows) {
             const cells = Array.from(row.querySelectorAll('td'));
-            const link = cells[0].querySelector('a');
+            const link = cells[1].querySelector('a');
             const [committeeName, reportName, filingYear, submittedDate] = cells.map(n => n.textContent);
             console.warn(`Getting ${submittedDate} ${reportName} for ${committeeName}`);
             const pdfFile =
@@ -45,7 +48,8 @@ async function main() {
                     [
                         committeeName,
                         filingYear,
-                        reportName.replace(' Report', ''),
+                        reportName.replace(' Report', '')
+                            .replace(/ \(20\d\d\)/, ''),
                         submittedDate.replace(/^(\d\d)\/(\d\d)\/(\d{4})$/, '$3$1$2'),
                     ].join(' ')
                 ) +
@@ -60,7 +64,8 @@ async function main() {
                     // File doesn't exist, so go on
                 }
             }
-            await browser.click(link);
+            await browser.click(link); // go to summary page
+            await browser.click('button.report-summary-pdf-div'); // get PDF
             const resource = browser.resources[browser.resources.length - 1];
             // A zombie bug causes the PDF content to be corrupted by conversion to a UTF-8 string,
             // so we have to make the request again with the request module
@@ -73,18 +78,14 @@ async function main() {
             fs.writeFileSync(pdfFile, pdfContent);
             console.warn(`Wrote ${pdfContent.length} bytes to ${pdfFile}`);
         }
-        page++;
-        const buttons = await browser.querySelectorAll('ul.pagination > li.page-item > button');
-        for (const button of buttons) {
-            if (+button.textContent === page) {
-                console.warn(`Clicking ${page}`);
-                await browser.click(button);
-                continue doTable;
-            }
+        console.warn(`Finished page ${page}`);
+        if (page < lastPage) {
+            const nextButton = await browser.querySelector('ul.pagination > li.page-item > button[aria-label=Next]');
+            console.warn('Clicking next');
+            await browser.click(nextButton);
         }
-        console.warn('Last page');
-        break;
     }
+    console.warn('Last page');
 }
 
 function getArgv() {

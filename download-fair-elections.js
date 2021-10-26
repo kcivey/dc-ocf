609b27f0de74eeb23ae316641933bdc60f30e3c2
Keith C. Ivey <keith@iveys.org>
2021-10-25 22:49:55 -0400

    adjust for format weirdness in Fair Elections

--- download-fair-elections.js
+++ download-fair-elections.js
@@ -49,7 +49,7 @@ async function main() {
                         committeeName,
                         filingYear,
                         reportName.replace(' Report', '')
-                            .replace(/ \(20\d\d\)/, ''),
+                            .replace(/(?: 20\d\d)? \(20\d\d\)/, ''),
                         submittedDate.replace(/^(\d\d)\/(\d\d)\/(\d{4})$/, '$3$1$2'),
                     ].join(' ')
                 ) +

6988a6071a9dfdcb027c57eab65a6acbbd3328ed
Keith C. Ivey <keith@iveys.org>
2023-11-05 11:53:09 -0500

    tweak geocode-csv.js

--- geocode-csv.js
+++ geocode-csv.js
@@ -6,6 +6,8 @@ const csvStringify = require('csv-stringify/lib/sync');
 const request = require('./lib/request');
 const marClient = require('dc-mar').createClient({request});
 const batchSize = 40;
+const stateField = 'Donor State';
+const addressField = 'Donor Address Line 1';
 const inputFile = process.argv[2];
 const outputFile = inputFile.replace(/(?=\.csv$)/i, '-with-wards');
 
@@ -42,23 +44,27 @@ function main() {
         input.pipe(parser);
 
         async function geocode() {
-            const addresses = batch.filter(r => r.state === 'DC').map(r => r.addr1);
-            console.warn(`Looking up ${addresses.length} from ${batch.length} records`);
-            const locations = await marClient.findLocationBatch(addresses);
-            let locationIndex = 0;
-            for (const record of batch) {
-                const location = record.state === 'DC' && locations[locationIndex] && locations[locationIndex][0];
-                newRecords.push({
-                    ...record,
-                    mar_confidence: location ? location.confidenceLevel() : 0,
-                    mar_address: location ? location.fullAddress() : '',
-                    ward: location ? location.ward() : '',
-                    smd: location ? location.smd() : '',
-                    latitude: location ? location.latitude() : '',
-                    longitude: location ? location.longitude() : '',
-                });
-                if (record.state === 'DC') {
-                    locationIndex++;
+            const addresses = batch.filter(r => r[stateField] === 'DC').map(r => r[addressField]);
+            console.warn(`Looking up ${addresses.length} DC records from ${batch.length} records`);
+            if (addresses.length) {
+                const locations = await marClient.findLocationBatch(addresses);
+                let locationIndex = 0;
+                for (const record of batch) {
+                    const location = record[stateField] === 'DC' && locations[locationIndex] &&
+                        locations[locationIndex][0];
+                    newRecords.push({
+                        ...record,
+                        mar_confidence: location ? location.confidenceLevel() : 0,
+                        mar_address: location ? location.fullAddress() : '',
+                        ward: location ? location.ward() : '',
+                        smd: location ? location.smd() : '',
+                        precinct: location ? location.precinct() : '',
+                        latitude: location ? location.latitude() : '',
+                        longitude: location ? location.longitude() : '',
+                    });
+                    if (record[stateField] === 'DC') {
+                        locationIndex++;
+                    }
                 }
             }
             batch = []; // eslint-disable-line require-atomic-updates

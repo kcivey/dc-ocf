7cddabbef2e27cf32ec4a4eb452034802a6d8f25
Keith C. Ivey <keith@iveys.org>
2024-02-03 18:04:40 -0500

    remove browserify (no longer used)

--- build.sh
+++ build.sh
@@ -9,6 +9,5 @@ rm dist/*
 cp -u src/*.{js,json,html} dist/
 echo 'Compiling and minifying JS'
 npx babel src/dc-campaign-finance.js | \
-    npx browserify - | \
     npx terser -c -m --toplevel --comments /Copyright/ \
     > dist/dc-campaign-finance.js

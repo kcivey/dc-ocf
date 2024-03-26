e04cb111d8848c25d63ba8dd161660b02ccdbc77
Keith C. Ivey <keith@iveys.org>
2024-03-26 17:55:58 -0400

    fix geometry garbling in round-geojson.sh

--- round-geojson.sh
+++ round-geojson.sh
@@ -2,6 +2,6 @@
 
 json -o json-0 -e 'this.features.forEach(function (f) {
         f.geometry.coordinates.forEach(function(poly) {
-            poly.forEach((c, i) => f.geometry.coordinates[i] = [+c[0].toFixed(6), +c[1].toFixed(6)]);
+            poly.forEach((c, i) => poly[i] = [+c[0].toFixed(6), +c[1].toFixed(6)]);
         });
     })'
